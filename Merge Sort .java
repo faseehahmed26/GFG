f15b39dbadfcd550147105fde0100d0e5f4907c3
faseeh007 <54709992+faseeh007@users.noreply.github.com>
2021-05-26 22:17:53 +0530

    Add files via upload

---
+++ Merge Sort .java
@@ -0,0 +1,133 @@
+import java.util.*;
+
+class Merge_Sort
+{
+    //method to print the elements of the array
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    
+
+	public static void main(String args[])
+	{
+	    //taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking testcases
+		int T = sc.nextInt();
+		while(T>0)
+		{
+		    //taking elements count
+			int n = sc.nextInt();
+			
+			//creating an object of class Merge_Sort
+			Merge_Sort ms = new Merge_Sort();
+			
+			//creating an array of size n
+			int arr[] = new int[n];
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+            
+			Solution g = new Solution();
+			
+			//calling the method mergeSort
+			g.mergeSort(arr,0,arr.length-1);
+
+            //calling the method printArray
+			ms.printArray(arr);
+		T--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+/* The task is to complete merge() which is used
+in below mergeSort() */
+// Merges two subarrays of arr[].  First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+class Solution
+{
+
+// Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
+
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    
+    //method for dividing the array into halves
+    public void mergeSort(int arr[], int l, int r)
+    {
+       
+        if (l < r)
+        {
+            int m = (l+r)/2;
+            mergeSort(arr, l, m);
+            mergeSort(arr , m+1, r);
+            merge(arr, l, m, r);
+        }
+    }
+}
