3a014ffaaa196646ebd1e47c777eca00a9b2b572
faseeh007 <faseehahmedrocks26@gmail.com>
2021-06-16 11:58:29 +0530

    2

--- Sorting Elements of an Array by Frequency.java
+++ Sorting Elements of an Array by Frequency.java
@@ -1,3 +1,4 @@
+//Initial Template for Java
 
 import java.util.*;
 import java.io.*;
@@ -32,72 +33,69 @@ class Driverclass
 
 // } Driver Code Ends
 
-
-//User function Template for Java
-class Element
+class comparator implements Comparator<Map.Entry<Integer,Integer>>
 {
-    int val;
-    int key;
-    
-    Element(int x,int y)
-    {
-        key = x;
-        val = y;
-    }
+    //Function used in sorting list elements first according to frequency
+    //and if it is same for both values, then smaller number comes first.
+	public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) 
+	{
+		if(o1.getValue() > o2.getValue())
+			return -1;
+		else if(o1.getValue() == o2.getValue()){
+			if(o2.getKey() < o1.getKey())
+			    return 1;
+			else
+			    return -1;
+		    
+		}
+		else if(o1.getValue() < o2.getValue())
+			return 1;
+		
+		return Integer.MIN_VALUE;
+	}
+	
 }
-
 class Solution
 {
     //Function to sort the array according to frequency of elements.
     static ArrayList<Integer> sortByFreq(int arr[], int n)
     {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        ArrayList<Integer> res = new ArrayList<Integer>();
+        //using map to keep count of the elements.
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
         
-        for(int i : arr)
-        {
-            if(map.containsKey(i))
-            map.put(i,map.get(i)+1);
-            else
-            map.put(i,1);
-        }
-        
-        Element el[] = new Element[map.size()];
-        int c = 0;
-        
-        for(Map.Entry<Integer,Integer> e : map.entrySet())
-        {
-            el[c] = new Element(e.getKey(),e.getValue());
-            c++;
-        }
-        
-        Arrays.sort(el,new Comparator<Element>(){
-            
-           public int compare(Element e1,Element e2)
-           {
-               if(e1.val>e2.val)
-               return 1;
-               else if(e1.val<e2.val)
-               return -1;
-               else 
-               {
-                   if(e1.key<e2.key)
-                   return 1;
-                   else
-                   return -1;
-               }
-           }
-        });
-        
-        for(int i = el.length-1;i>=0;i--)
-        {
-            while(el[i].val>0)
-            {
-                res.add(el[i].key);
-                el[i].val--;
-            }
-        }
-        
-        return res;
+        //storing the frequency of each element in map.
+		for(int i = 0; i < n; i++)
+		{
+				int num = arr[i];
+				
+				if(map.containsKey(num))
+				{
+					map.put(num, map.get(num) + 1);
+				}
+				else
+				{
+					map.put(num, 0);
+				}
+		}
+		
+		//copying the elements and their count from map to list.	
+		List<Map.Entry<Integer,Integer>> set = new ArrayList<Map.Entry<Integer,Integer>> (map.entrySet());
+		
+		//sorting the list according to frequency.
+		Collections.sort(set,new comparator());
+		ArrayList <Integer> ans = new ArrayList<Integer>();
+		
+		for(int i = 0;i<map.size();i++)
+		{
+			int count = set.get(i).getValue();
+			//storing elements as many times as their count in output list.
+			while(count >= 0)
+			{
+					ans.add(set.get(i).getKey());
+					count -- ;
+			}
+		}
+		//returning the output.
+        return ans;
     }
 }
