a1374a077e20e17be7e2531938042604653acb08
faseeh007 <faseehahmedrocks26@gmail.com>
2021-06-27 17:20:16 +0530

    2

---
+++ Chocolate Distribution Problem .java
@@ -0,0 +1,48 @@
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            long n = sc.nextLong();
+            ArrayList<Long> arr = new ArrayList<Long>();
+            for(int i = 0;i<n;i++)
+                {
+                    long x = sc.nextInt();
+                    arr.add(x);
+                }
+            long m = sc.nextLong();
+            
+            Solution ob = new Solution();
+            
+    		System.out.println(ob.findMinDiff(arr,n,m));
+        }
+        
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long findMinDiff (ArrayList<Long> a, long n, long m)
+    {
+        // your code here
+        Collections.sort(a);
+        int i;
+        long l=Integer.MAX_VALUE;
+        for(i=0;i<=n-m;i++){
+            l=Math.min(a.get((int)m+i-1)-a.get(i),l);
+        }
+        return l;
+    }
+}
